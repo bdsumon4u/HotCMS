24ebe66a273966b3fabc185ca2fcd57f48192175
Sumon Ahmed <bdsumon4u@gmail.com>
2025-01-03 17:30:27 +0600

    Add HasFactory trait to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Str;
 use Wave\User as WaveUser;
 use Illuminate\Notifications\Notifiable;
@@ -9,7 +10,7 @@ use Wave\Traits\HasProfileKeyValues;
 
 class User extends WaveUser
 {
-    use Notifiable, HasProfileKeyValues;
+    use HasFactory, Notifiable, HasProfileKeyValues;
 
     public $guard_name = 'web';
 
